9595cc2a0795b4f7b829e8ec48f6f1378110cab6
Youhei Oonuma <oonuma617@gmail.com>
2020-10-28 20:20:37 +0900

    image filter

--- bin/tw1/twitter.dart
+++ bin/tw1/twitter.dart
@@ -11,27 +11,40 @@ void main() async {
     ACCESS_TOKEN,
     ACCESS_TOKEN_SECRET,
   );
-  final response =
-      await twitter.request('GET', 'favorites/list.json?include_entities=true');
+  final response = await twitter.request(
+      'GET', 'favorites/list.json?include_entities=true&count=100');
 //  final response = await twitter.request('GET', 'statuses/home_timeline.json');
 
   print(response.body);
 
   List<dynamic> jsonResponse = json.decode(response.body);
+  var twitterMediaList = <_twitterMedia>[];
   for (var tw in jsonResponse) {
     final extendedEntities = tw['extended_entities'];
     if (extendedEntities != null) {
       final media = extendedEntities['media'] as List<dynamic>;
       if (media != null) {
-        print('------------------------------------------------');
-        print(tw['user']['name']);
-        print(tw['text']);
         for (var me in media) {
-          print(me['media_url_https']);
+          twitterMediaList.add(_twitterMedia(me['media_url_https']));
         }
       }
     }
   }
 
   twitter.close();
+
+  for (var value in twitterMediaList) {
+    print(value);
+  }
+}
+
+class _twitterMedia {
+  final String mediaUrl;
+
+  _twitterMedia(this.mediaUrl);
+
+  @override
+  String toString() {
+    return '_twitterMedia{mediaUrl: $mediaUrl}';
+  }
 }
