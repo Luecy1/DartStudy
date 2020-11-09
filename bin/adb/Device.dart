c5da24adfbedfa998a9848bc75c8ba92be69004e
Youhei Oonuma <oonuma617@gmail.com>
2020-11-09 15:32:33 +0900

    Device

--- bin/adb/Device.dart
+++ bin/adb/Device.dart
@@ -1,15 +1,43 @@
 import 'dart:io';
 
 void main() async {
-  final devicesResult = await Process.run('adb', ['devices', '-l']);
-
-  if (devicesResult.exitCode != 0) {
-    throw Exception('adb error no devices');
-  }
-
-  (devicesResult.stdout as String).split('\r\n').where((element) {
-    element.matchAsPrefix("list");
-  }).forEach((element) {
+  final deviceList = await getDeviceList();
+  deviceList.forEach((element) {
     print(element);
   });
+}
+
+Future<List<Device>> getDeviceList() async {
+  final adb = await Process.run('adb', ['devices', '-l']);
+  if (adb.exitCode == 0) {
+    final result = (adb.stdout as String);
+
+    final deviceList = result
+        .split('\r\n')
+        .where((element) {
+          return !element.contains('List of devices attached');
+        })
+        .where((element) => element.isNotEmpty)
+        .map((e) {
+          final split = e.split(RegExp(' +'));
+          final serialId = split[0];
+          final modelName = split[3].split(':')[1];
+          return Device(serialId, modelName);
+        });
+    return deviceList.toList();
+  } else {
+    throw Exception('adb command error \n${adb.stderr}');
+  }
+}
+
+class Device {
+  final String serial;
+  final String modelName;
+
+  Device(this.serial, this.modelName);
+
+  @override
+  String toString() {
+    return 'Device{serial: $serial, modelName: $modelName}';
+  }
 }
